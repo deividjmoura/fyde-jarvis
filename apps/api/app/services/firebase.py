622736f91ac103421ae3eca9f15828d02f4e0bee
Deivid Moura <dejesusdemouradeivid@gmail.com>
2026-05-17 16:13:50 -0300

    fix: resolve firebase auth import conflict

--- apps/api/app/services/firebase.py
+++ apps/api/app/services/firebase.py
@@ -1,8 +1,32 @@
+import json
+
+import firebase_admin
+
+from firebase_admin import credentials
+from firebase_admin import auth as firebase_auth
+
+from app.core.config import settings
+
+
+firebase_credentials = json.loads(
+    settings.FIREBASE_CREDENTIALS
+)
+
+cred = credentials.Certificate(
+    firebase_credentials
+)
+
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
+
+
 def verify_firebase_token(token: str):
-    decoded_token = auth.verify_id_token(token)
+    """
+    Verifica token JWT do Firebase
+    """
+
+    decoded_token = firebase_auth.verify_id_token(token)
 
     print("DECODED:", decoded_token)
 
     return decoded_token
-
-    print("BACKEND PROJECT:", firebase_credentials.get("project_id"))
